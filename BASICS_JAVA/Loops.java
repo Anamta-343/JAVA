f3a118461c1c92bf7baf99344b5016a9457da43a
Anamta-343 <anamtakoty334@gmail.com>
2025-07-25 23:16:36 +0530

    Initial commit

---
+++ BASICS_JAVA/Loops.java
@@ -0,0 +1,33 @@
+public class Loops {
+    public static void main(String args[]){
+
+        // for(int i=0;i<=10;i++){                               # FOR LOOP
+        //     System.out.print(i + " ");
+        // }
+
+        // int i=0;                                              # While Loop
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i+=1;
+        // }
+        
+        // int i=0;                                               # DO WHILE
+        // do{
+        //     System.out.println(i);
+        //     i+=1;
+        // }while(i<=10);
+
+        int n=4;
+        int sum=0;
+        for(int i=1 ;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+    
+    
+}
---
+++ BASICS_JAVA/variables.java
@@ -0,0 +1,39 @@
+// package JAVA;
+// import java.util.*;
+
+// public class variables {
+//     public static void main(String args[]){
+     //    System.out.println("HELLO WORLD");                         # OUTPUT
+    //    System.out.println("*\n**\n***\n****");
+
+    // String name ="Tony Stark";                                    # VARIABLE
+    // int a =25;
+    // int b =47;
+    // int age =21;
+    // double price=99.99;
+    // b=34;
+    // name="ironman";  
+
+    // int a=10;                                                     # DATA TYPES
+    // int b=25;
+    // int sum = a+b;
+    // int diff = b-a;
+    // int mult = a*b;
+    // System.out.println(sum);
+    // System.out.println(diff);
+    // System.out.println(mult);
+
+    // Scanner sc =new Scanner(System.in);                            # INPUT
+    // String name = sc.nextLine();
+    // System.out.println(name);
+
+    // Scanner sc =new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int sum = a+b;
+    // System.out.println(sum);
+
+   
+    
+//     }
+// }
